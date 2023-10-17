e275948bd410b5ce781f419b8b82fb204fa41d80
Kevin Lievano <lvrd07@gmail.com>
2023-10-17 10:52:08 -0500

    cargue de todos los archivos de cinemapedia

---
+++ lib/infrastructure/datasources/isar_datasource.dart
@@ -0,0 +1,69 @@
+import 'package:isar/isar.dart';
+
+import '../../domain/datasources/local_storage_datasource.dart';
+import '../../domain/entities/movie.dart';
+
+
+
+class IsarDatasource extends LocalStorageDatasource {
+  
+  late Future<Isar> db;
+
+  IsarDatasource() {
+    db = openDB();
+  }
+
+  Future<Isar> openDB() async {
+    if ( Isar.instanceNames.isEmpty ) {
+      return await Isar.open([ MovieSchema ], directory: '' );
+    }
+
+    return Future.value(Isar.getInstance());
+  }
+
+
+  @override
+  Future<bool> isMovieFavorite(int movieId) async {
+    final isar = await db;
+
+    final Movie? isFavoriteMovie = await isar.movies
+      .filter()
+      .idEqualTo(movieId)
+      .findFirst();
+
+    return isFavoriteMovie != null;
+  }
+
+  @override
+  Future<void> toggleFavorite(Movie movie) async {
+    
+    final isar = await db;
+
+    final favoriteMovie = await isar.movies
+      .filter()
+      .idEqualTo(movie.id)
+      .findFirst();
+
+    if ( favoriteMovie != null ) {
+      // Borrar
+      isar.writeTxn(() => isar.movies.delete( favoriteMovie.isarId! ));
+      return;
+    }
+
+    // Insertar
+    isar.writeTxn(() => isar.movies.put(movie));
+
+  }
+
+  @override
+  Future<List<Movie>> loadMovies({int limit = 10, offset = 0}) async {
+    
+    final isar = await db;
+
+    return isar.movies.where()
+      .offset(offset)
+      .limit(limit)
+      .findAll();
+  }
+
+}
